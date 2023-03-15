5f32ac53fae80ab14fc1cab0c815218ba35fb32f
shu1r0 <shu1r0.net@gmail.com>
2023-03-15 16:28:05 +0900

    add go path

--- .config/bash/ubuntu/env.sh
+++ .config/bash/ubuntu/env.sh
@@ -1,3 +1,8 @@
 export VAGRANT_DEFAUL_PROVIDER="virtualbox"
 
 export PATH="$HOME/.cargo/bin:$PATH"
+
+# go env
+export PATH="/usr/local/go/bin:$PATH"
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$PATH"
